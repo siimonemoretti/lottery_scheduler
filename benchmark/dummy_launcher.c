ab6c62c2ccb314c2b6021e4d61469ec2800aa038
Simone Moretti <s345922@studenti.polito.it>
2025-06-24 09:43:03 +0000

    Fix dummy launcher

--- benchmark/dummy_launcher.c
+++ benchmark/dummy_launcher.c
@@ -3,76 +3,69 @@
  * Childs will execute the dummy_program process.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sched.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+ #include <sched.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <unistd.h>
  
-#ifndef SCHED_LOTTERY
-#include "linux/sched.h"
-#endif
+ #ifndef SCHED_LOTTERY
+ #include "linux/sched.h"
+ #endif
  
-
-void start_dummy_with_lottery(char *arg) {
-   pid_t pid = fork();
-   if (pid == 0) {
-		// Write to file the PID of the child process to a new line
-		FILE *file = fopen(arg, "a");
-		if (file == NULL) {
-			perror("Failed to open file");
-			exit(EXIT_FAILURE);
-		} else {
-			fprintf(file, "%d\n", getpid());
-			fclose(file);
-		}
-	   // In child: run dummy_program
-	   execl("./dummy_program", "dummy_program", NULL);
-	   perror("execl failed");
-	   exit(EXIT_FAILURE);
-   } else if (pid < 0) {
-	   perror("fork failed");
-   }
-}
-
-int main(int argc, char *argv[]) {
+ void start_dummy_with_lottery() {
+	pid_t pid = fork();
+	if (pid == 0) {
+	  // In child: run dummy_program
+	  execl("./dummy", "dummy", NULL);
+	  perror("execl failed");
+	  exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+	  perror("fork failed");
+	}
+ }
+ 
+ int main(int argc, char *argv[]) {
+	if (argc != 2) {
+	  fprintf(stderr, "Usage: %s <number_of_children>\n", argv[0]);
+	  exit(EXIT_FAILURE);
+	}
+	int num_children = atoi(argv[1]);
+	if (num_children <= 0) {
+	  fprintf(stderr, "Number of children must be a positive integer.\n");
+	  exit(EXIT_FAILURE);
+	}
+ 
 	struct sched_param param;
 	int policy;
-	
-	#ifdef SCHED_LOTTERY
-	printf("SCHED_LOTTERY is defined\n");
-	#else
-	printf("SCHED_LOTTERY is not defined\n");
-	#endif
-	
+ 
 	// Display current scheduling policy before changing
 	policy = sched_getscheduler(0);
-	printf("Initial scheduling policy: %d\n", policy);
-	
+ 
 	// Set the scheduling priority
 	param.sched_priority = 0;
-	
+ 
 	// Set the scheduling policy to LOTTERY
 	int r = sched_setscheduler(0, SCHED_LOTTERY, &param);
 	if (r == -1) {
-	   perror("sched_setscheduler");
-	   exit(EXIT_FAILURE);
+	  perror("sched_setscheduler");
+	  exit(EXIT_FAILURE);
 	}
-	
+ 
 	// Get and print the new scheduling policy
 	policy = sched_getscheduler(0);
 	printf("New scheduling policy: %d\n", policy);
-
-   // Fork two child processes
-   start_dummy_with_lottery(argv[1]);
-   start_dummy_with_lottery(argv[1]);
-	start_dummy_with_lottery(argv[1]);
+ 
+	// Fork N child processes, based on argv[1]
+	for (int i = 0; i < num_children; i++) {
+	  start_dummy_with_lottery();
+	}
+ 
 	// Wait for child processes to finish
-   wait(NULL);
-   wait(NULL);
-	wait(NULL);
-   
-   return 0;
-}
+	// wait(NULL);
+	// wait(NULL);
+ 
+	return 0;
+ }
